d0aacce7ea7f52086df6eaac1fbaf87fee87bc2b
vagnersaller <vsaller@yahoo.com>
2020-11-19 09:02:42 -0300

    ui: adjust namespace menu

--- ui/src/components/namespace/NamespaceAdd.vue
+++ ui/src/components/namespace/NamespaceAdd.vue
@@ -1,61 +1,65 @@
 <template>
   <fragment>
-    <v-btn
-      small
-      class="v-btn--active"
-      text
-      color="primary"
-      outlined
-      @click="dialog = !dialog"
+    <v-list-item
+      @click="dialog= !dialog"
     >
-      Add Namespace
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      max-width="450"
-      @click:outside="cancel"
-    >
-      <v-card>
-        <v-card-title class="headline grey lighten-2 text-center">
-          Enter Namespace
-        </v-card-title>
-        <ValidationObserver
-          ref="obs"
-          v-slot="{ passes }"
-        >
-          <v-card-text class="caption mb-0">
-            <ValidationProvider
-              v-slot="{ errors }"
-              name="namespace"
-              rules="required|rfc1123"
-            >
-              <v-text-field
-                v-model="namespace"
-                label="Namespace"
-                :error-messages="errors"
-                require
-              />
-            </ValidationProvider>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              text
-              @click="cancel"
-            >
-              Close
-            </v-btn>
-            <v-btn
-              color="primary"
-              text
-              @click="passes(addNamespace)"
-            >
-              Add
-            </v-btn>
-          </v-card-actions>
-        </ValidationObserver>
-      </v-card>
-    </v-dialog>
+      <v-list-item-icon>
+        <v-icon>mdi-plus-box</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>
+          Create Namespace
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item-title>
+      <v-dialog
+        v-model="dialog"
+        max-width="450"
+        @click:outside="cancel"
+      >
+        <v-card>
+          <v-card-title class="headline grey lighten-2 text-center">
+            Enter Namespace
+          </v-card-title>
+          <ValidationObserver
+            ref="obs"
+            v-slot="{ passes }"
+          >
+            <v-card-text class="caption mb-0">
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="namespace"
+                rules="required|rfc1123"
+              >
+                <v-text-field
+                  v-model="namespace"
+                  label="Namespace"
+                  :error-messages="errors"
+                  require
+                />
+              </ValidationProvider>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                text
+                @click="cancel"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                @click="passes(addNamespace)"
+              >
+                Add
+              </v-btn>
+            </v-card-actions>
+          </ValidationObserver>
+        </v-card>
+      </v-dialog>
+    </v-list-item-title>
   </fragment>
 </template>
 
--- ui/src/components/namespace/NamespaceMenu.vue
+++ ui/src/components/namespace/NamespaceMenu.vue
@@ -6,9 +6,7 @@
     >
       <template #activator="{ on }">
         <v-chip v-on="on">
-          <v-icon
-            left
-          >
+          <v-icon left>
             mdi-server
           </v-icon>
           {{ namespace.name }}
@@ -18,21 +16,12 @@
         </v-chip>
       </template>
       <v-card>
-        <v-list-item three-line>
+        <v-subheader>Tenant ID</v-subheader>
+        <v-list-item>
           <v-list-item-content>
-            <v-list-item-title
-              data-test="tenantID-field"
-              class="mb-1"
-            >
-              Tenant ID
-            </v-list-item-title>
             <v-list-item-subtitle>
               <v-chip>
-                <span
-                  data-test="tenantID-text"
-                >
-                  {{ tenant }}
-                </span>
+                <span data-test="tenantID-text">{{ tenant }}</span>
                 <v-icon
                   v-clipboard="tenant"
                   v-clipboard:success="() => {
@@ -46,110 +35,57 @@
             </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-        <v-list-group>
-          <template #activator>
-            <v-list-item-content>
-              <v-list-item-title>
-                Namespace List
-              </v-list-item-title>
-            </v-list-item-content>
-          </template>
-          <v-list-item
-            v-show="show"
-          >
-            <v-list-item-title>
-              <NamespaceAdd />
-            </v-list-item-title>
-          </v-list-item>
-          <v-virtual-scroll
-            :height="150"
-            item-height="40"
-            :items="namespaces"
-          >
-            <template #default="{ item }">
-              <v-list-item :key="item.tenant_id">
-                <v-row>
-                  <v-col
-                    v-if="formatName(item.name).mode"
-                    class="mt-1"
-                  >
-                    <v-tooltip
-                      bottom
-                    >
-                      <template
-                        #activator="{ on }"
-                      >
-                        <v-list-item-title
-                          v-on="on"
-                        >
-                          {{ formatName(item.name).name }}
-                        </v-list-item-title>
-                      </template>
-                      <span>
-                        {{ item.name }}
-                      </span>
-                    </v-tooltip>
-                  </v-col>
-                  <v-col
-                    v-else
-                  >
+        <v-list flat>
+          <v-subheader>Namespaces</v-subheader>
+          <v-list-item-group>
+            <v-virtual-scroll
+              :height="150"
+              item-height="60"
+              :items="namespaces"
+            >
+              <template #default="{ item }">
+                <v-list-item
+                  :key="item.tenant_id"
+                  @click="switchIn(item.tenant_id)"
+                >
+                  <v-list-item-icon>
+                    <v-icon>mdi-login</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
                     <v-list-item-title>
                       {{ item.name }}
                     </v-list-item-title>
-                  </v-col>
-                  <v-spacer />
-                  <v-spacer />
-                  <v-col>
-                    <v-btn
-                      small
-                      class="v-btn--active"
-                      text
-                      color="primary"
-                      @click="switchIn(item.tenant_id)"
-                    >
-                      <v-tooltip
-                        bottom
-                      >
-                        <template #activator="{ on }">
-                          <v-icon
-                            v-on="on"
-                          >
-                            mdi-sync
-                          </v-icon>
-                        </template>
-                        <span>
-                          Switch namespace
-                        </span>
-                      </v-tooltip>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-list-item>
-            </template>
-          </v-virtual-scroll>
-        </v-list-group>
-        <v-list-item
-          key="settings"
-        >
-          <v-list-item-title>
-            <v-btn
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-virtual-scroll>
+          </v-list-item-group>
+          <v-divider />
+          <v-list-item-group
+            v-model="model"
+          >
+            <NamespaceAdd />
+            <v-divider />
+            <v-list-item
               to="/settings/namespace-manager"
-              class="v-btn--active"
-              text
-              color="primary"
-              small
             >
-              Namespace manager
-            </v-btn>
-          </v-list-item-title>
-        </v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-cog</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Settings
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
       </v-card>
     </v-menu>
   </fragment>
 </template>
 
 <script>
-
 import NamespaceAdd from '@/components/namespace/NamespaceAdd';
 
 export default {
@@ -157,6 +93,12 @@ export default {
 
   components: {
     NamespaceAdd,
+  },
+
+  data() {
+    return {
+      model: 1,
+    };
   },
 
   computed: {
@@ -191,7 +133,8 @@ export default {
 
   methods: {
     async getNamespaces() {
-      try { // load namespaces
+      try {
+        // load namespaces
         await this.$store.dispatch('namespaces/fetch');
         await this.$store.dispatch('namespaces/get', this.tenant);
       } catch {
@@ -209,13 +152,6 @@ export default {
         this.$store.dispatch('snackbar/showSnackbarErrorLoading', this.$errors.namespaceSwitch);
       }
     },
-
-    formatName(name, limit = 11) {
-      const formatObj = { name, mode: true };
-      if (name.length > limit) formatObj.name = `${name.slice(0, limit - 3)}...`; formatObj.format = false;
-      return formatObj;
-    },
   },
 };
-
 </script>
