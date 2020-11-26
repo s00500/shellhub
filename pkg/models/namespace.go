bf76281a22e0773ce3e8499217071e55e1c46fc7
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-11-26 16:02:58 -0300

    api: merge members and member_names in same property

--- pkg/models/namespace.go
+++ pkg/models/namespace.go
@@ -1,14 +1,18 @@
 package models
 
 type Namespace struct {
-	Name        string             `json:"name"  validate:"required,hostname_rfc1123"`
-	Owner       string             `json:"owner"`
-	TenantID    string             `json:"tenant_id" bson:"tenant_id,omitempty"`
-	Members     []string           `json:"members" bson:"members"`
-	MemberNames []string           `json:"member_names,omitempty" bson:"member_names",omitempty`
-	Settings    *NamespaceSettings `json:"settings"`
+	Name     string             `json:"name"  validate:"required,hostname_rfc1123"`
+	Owner    string             `json:"owner"`
+	TenantID string             `json:"tenant_id" bson:"tenant_id,omitempty"`
+	Members  interface{}        `json:"members" bson:"members"`
+	Settings *NamespaceSettings `json:"settings"`
 }
 
 type NamespaceSettings struct {
 	SessionRecord bool `json:"session_record" bson:"session_record,omitempty"`
+}
+
+type Member struct {
+	ID   string `json:"id" bson:"id"`
+	Name string `json:"name,omitempty" bson:"-"`
 }
