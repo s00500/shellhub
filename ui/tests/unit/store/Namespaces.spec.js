3f62b2af878a10bdc6cfeec74007c335dcbac723
vagnersaller <vsaller@yahoo.com>
2020-11-27 15:31:13 -0300

    ui: change member list in template

--- ui/tests/unit/store/Namespaces.spec.js
+++ ui/tests/unit/store/Namespaces.spec.js
@@ -6,25 +6,25 @@ describe('Namespace', () => {
     {
       name: 'namespace1',
       owner: 'user1',
-      member_names: ['user3', 'user4', 'user5'],
+      members: [{ name: 'user3' }, { name: 'user4' }, { name: 'user5' }],
       tenant_id: 'a736a52b-5777-4f92-b0b8-e359bf484713',
     },
     {
       name: 'namespace2',
       owner: 'user1',
-      member_names: ['user3', 'user4'],
+      members: [{ name: 'user3' }, { name: 'user4' }],
       tenant_id: 'a736a52b-5777-4f92-b0b8-e359bf484714',
     },
     {
       name: 'namespace3',
       owner: 'user1',
-      member_names: ['user6', 'user7', 'user8'],
+      members: [{ name: 'user6' }, { name: 'user 7' }, { name: 'user 8' }],
       tenant_id: 'a736a52b-5777-4f92-b0b8-e359bf484715',
     },
     {
       name: 'namespace4',
       owner: 'user1',
-      member_names: ['user6', 'user7'],
+      members: [{ name: 'user6' }, { name: 'user7' }],
       tenant_id: 'a736a52b-5777-4f92-b0b8-e359bf484716',
     },
   ];
@@ -32,7 +32,7 @@ describe('Namespace', () => {
   const namespace = {
     name: 'namespace3',
     owner: 'user1',
-    member_names: ['user6', 'user7', 'user8'],
+    members: [{ name: 'user6' }, { name: 'user7' }, { name: 'user8' }],
     tenant_id: 'a736a52b-5777-4f92-b0b8-e359bf484715',
   };
 
@@ -52,12 +52,12 @@ describe('Namespace', () => {
     expect(store.getters['namespaces/get']).toEqual(namespace);
   });
   it('Verify changed member list for removeMember mutation', () => {
-    const lengthListOfMembers = namespace.member_names.length;
-    const lastUsername = namespace.member_names[lengthListOfMembers - 1];
+    const lengthListOfMembers = namespace.members.length;
+    const lastUsername = namespace.members[lengthListOfMembers - 1];
     store.commit('namespaces/removeMember', lastUsername);
-    expect(store.getters['namespaces/get'].member_names).toHaveLength(lengthListOfMembers - 1);
-    store.getters['namespaces/get'].member_names.forEach((member) => {
-      expect(member === lastUsername).toBeFalsy();
+    expect(store.getters['namespaces/get'].members).toHaveLength(lengthListOfMembers - 1);
+    store.getters['namespaces/get'].members.forEach((member) => {
+      expect(member.name === lastUsername).toBeFalsy();
     });
   });
   it('Verify changed namespaces list for removeNamespace mutation', () => {
