7baa8210169ec6928350d2b2f53f79be604d922f
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-12-03 17:24:40 -0300

    api: move ensureIndexes to migrations

--- api/store/mongo/migrations.go
+++ api/store/mongo/migrations.go
@@ -207,7 +207,7 @@ var migrations = []migrate.Migration{
 			return err
 		},
 	},
-{
+	{
 		Version: 12,
 		Up: func(db *mongo.Database) error {
 			mod := mongo.IndexModel{
@@ -229,14 +229,109 @@ var migrations = []migrate.Migration{
 			return err
 		},
 	},
+	{
+		Version: 13,
+		Up: func(db *mongo.Database) error {
+			mod := mongo.IndexModel{
+				Keys:    bson.D{{"uid", 1}},
+				Options: options.Index().SetName("uid").SetUnique(true),
+			}
+			_, err := db.Collection("devices").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"last_seen", 1}},
+				Options: options.Index().SetName("last_seen").SetExpireAfterSeconds(30),
+			}
+			_, err = db.Collection("connected_devices").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"uid", 1}},
+				Options: options.Index().SetName("uid").SetUnique(false),
+			}
+			_, err = db.Collection("connected_devices").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"uid", 1}},
+				Options: options.Index().SetName("uid").SetUnique(true),
+			}
+			_, err = db.Collection("sessions").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"last_seen", 1}},
+				Options: options.Index().SetName("last_seen").SetExpireAfterSeconds(30),
+			}
+			_, err = db.Collection("active_sessions").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"uid", 1}},
+				Options: options.Index().SetName("uid").SetUnique(false),
+			}
+			_, err = db.Collection("active_sessions").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"username", 1}},
+				Options: options.Index().SetName("username").SetUnique(true),
+			}
+			_, err = db.Collection("users").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+
+			mod = mongo.IndexModel{
+				Keys:    bson.D{{"tenant_id", 1}},
+				Options: options.Index().SetName("tenant_id").SetUnique(true),
+			}
+			_, err = db.Collection("users").Indexes().CreateOne(context.TODO(), mod)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		Down: func(db *mongo.Database) error {
+			if _, err := db.Collection("devices").Indexes().DropOne(context.TODO(), "uid"); err != nil {
+				return err
+			}
+			if _, err := db.Collection("connected_devices").Indexes().DropOne(context.TODO(), "last_seen"); err != nil {
+				return err
+			}
+			if _, err := db.Collection("connected_devices").Indexes().DropOne(context.TODO(), "uid"); err != nil {
+				return err
+			}
+			if _, err := db.Collection("sessions").Indexes().DropOne(context.TODO(), "uid"); err != nil {
+				return err
+			}
+			if _, err := db.Collection("active_sessions").Indexes().DropOne(context.TODO(), "last_seen"); err != nil {
+				return err
+			}
+			if _, err := db.Collection("users").Indexes().DropOne(context.TODO(), "username"); err != nil {
+				return err
+			}
+			_, err := db.Collection("users").Indexes().DropOne(context.TODO(), "tenant_id")
+			return err
+		},
+	},
 }
 
 func ApplyMigrations(db *mongo.Database) error {
 	m := migrate.NewMigrate(db, migrations...)
 
-	if err := m.Up(migrate.AllAvailable); err != nil {
-		return err
-	}
-
-	return EnsureIndexes(db)
+	return m.Up(migrate.AllAvailable)
 }
